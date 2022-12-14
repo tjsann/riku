fbff4423a739ce3053690f4366a47b228040490e
jijijijiji <106698290+tjsann@users.noreply.github.com>
2022-12-14 23:39:29 +0900

    Update zaseki.js

--- eiga/ej/zaseki.js
+++ eiga/ej/zaseki.js
@@ -4,33 +4,33 @@ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-chi
 
 //空席の場合選択
 if(document.getElementsByClassName("default seat-B-11").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-11.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-11 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-12").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-12.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-12 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-10").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-10.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-10 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-13").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-13.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-13 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-9").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-9.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-9 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-14").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-14.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-14 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-8").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-8.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-8 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-15").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-15.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-15 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-7").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-7.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-7 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-16").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-16.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-16 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-6").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-6.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-6 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-17").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-17.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-17 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-5").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-15.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-5 > div.seat-label").click();
 }else if(document.getElementsByClassName("default seat-B-18").length !=0){
- document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-A > div.seat.seat-A-15.default").click();
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-18 > div.seat-label").click();
 }
 //次画面へ
 document.querySelector("#contents > app-purchase-cinema-seat > div > div.buttons.mx-auto.text-center > button.btn.btn-primary.btn-block.py-3.mb-3").click();
