3fb03f2d9dfaad56bb45ba13599089d6c3847cba
jijijijiji <106698290+tjsann@users.noreply.github.com>
2022-12-14 23:53:05 +0900

    Create zaseki_b12.js

---
+++ eiga/ej/zaseki_b12.js
@@ -0,0 +1,9 @@
+
+//座席選択モード
+document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div:nth-child(2)").click();
+
+//空席の場合選択
+ document.querySelector("#contents > app-purchase-cinema-seat > div > div:nth-child(1) > div > div > div.col-lg-8 > app-screen > div.position-relative.bg-white > div > div.screen-scroll > div > div.object.seat-B > div.default.seat.seat-B-12 > div.seat-label").click();
+
+//次画面へ
+document.querySelector("#contents > app-purchase-cinema-seat > div > div.buttons.mx-auto.text-center > button.btn.btn-primary.btn-block.py-3.mb-3").click();
